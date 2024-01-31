68800260ae13a00c9511a77ab29ff9b440d9a763
degradka <degradka@gmail.com>
2024-01-31 20:56:35 +0300

    game initialization

--- rd-132211/src/main.c
+++ rd-132211/src/main.c
@@ -1,1 +1,98 @@
- 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+GLFWwindow* window;
+
+void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+    if (action == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+    }
+}
+
+void init() {
+    int width = 1024;
+    int height = 768;
+
+    // Initialize GLFW
+    if (!glfwInit()) {
+        fprintf(stderr, "Failed to initialize GLFW\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // Set GLFW window hints
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+
+    // Create a windowed mode window and its OpenGL context
+    window = glfwCreateWindow(width, height, "RubyDung", NULL, NULL);
+    if (!window) {
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+
+    // Make the window's context current
+    glfwMakeContextCurrent(window);
+
+    // Enable VSync
+    glfwSwapInterval(1);
+
+    // Enable alpha blending
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+    // Set the clear color
+    glClearColor(0.5f, 0.8f, 1.0f, 0.0f);
+
+    // Enable depth testing
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LEQUAL);
+
+    // Set up perspective
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(70, width / (float)height, 0.05F, 1000);
+    glMatrixMode(GL_MODELVIEW);
+
+    // Grab the mouse
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+    // Set the key callback function
+    glfwSetKeyCallback(window, keyCallback);
+}
+
+void destroy() {
+    // Terminate GLFW
+    glfwDestroyWindow(window);
+    glfwTerminate();
+}
+
+int main(void) {
+    // Initialize GLFW
+    if (!glfwInit()) {
+        fprintf(stderr, "Failed to initialize GLFW\n");
+        return EXIT_FAILURE;
+    }
+
+    // Initialize the game
+    init();
+
+    // Loop until the user closes the window
+    while (!glfwWindowShouldClose(window)) {
+        // Render here
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        // Update the display
+        glfwSwapBuffers(window);
+
+        // Poll for and process events
+        glfwPollEvents();
+    }
+
+    // Destroy I/O and save game
+    destroy();
+
+    return EXIT_SUCCESS;
+}
