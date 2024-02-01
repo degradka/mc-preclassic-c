d78d18c0f70ea8b1105368b5628caf8c35535163
degradka <degradka@gmail.com>
2024-02-01 19:33:26 +0300

    add motion to player movement

--- rd-132211/src/player.c
+++ rd-132211/src/player.c
@@ -1,5 +1,8 @@
 #include "player.h"
 
+#define degToRad(angleInDegrees) ((angleInDegrees) * M_PI / 180.0)
+#define radToDeg(angleInRadians) ((angleInRadians) * 180.0 / M_PI)
+
 void Player_init(Player* player, Level* level) {
     player->level = level;
     Player_resetPosition(player);
@@ -26,12 +29,16 @@ void Player_resetPosition(Player* player) {
     Player_setPosition(player, x, y, z);
 }
 
-void Player_turn(Player* player, float motionX, float motionY) {
-    player->yRotation += motionX * 0.15F;
-    player->xRotation -= motionY * 0.15F;
+void Player_turn(Player* player, float x, float y) {
+    player->yRotation += x * 0.15F;
+    player->xRotation -= y * 0.15F;
 }
 
 void Player_move(Player* player, double x, double y, double z) {
+    double prevX = x;
+    double prevY = y;
+    double prevZ = z;
+
     // Get surrounded tiles
     ArrayList_AABB aABBs = Level_getCubes(player->level, &player->boundingBox);
 
@@ -53,15 +60,23 @@ void Player_move(Player* player, double x, double y, double z) {
     }
     AABB_move(&player->boundingBox, 0.0F, 0.0F, z);
 
+    // Update on ground state
+    player->onGround = prevY != y && prevY < 0.0F;
+
+    // Stop motion on collision
+    if (prevX != x) player->motionX = 0.0;
+    if (prevY != y) player->motionY = 0.0;
+    if (prevZ != z) player->motionZ = 0.0;
+
     // Move the actual player position
-    player->x = (player->boundingBox.minX + player->boundingBox.maxX) / 2.0F;
+    player->x = (player->boundingBox.minX + player->boundingBox.maxX) / 2.0;
     player->y = player->boundingBox.minY + 1.62F;
-    player->z = (player->boundingBox.minZ + player->boundingBox.maxZ) / 2.0F;
+    player->z = (player->boundingBox.minZ + player->boundingBox.maxZ) / 2.0;
 }
 
 void Player_tick(Player* player, GLFWwindow* window) {
-    float motionX = 0.0F;
-    float motionZ = 0.0F;
+    float forward = 0.0F;
+    float vertical = 0.0F;
 
     // Player movement using GLFW
     if (glfwGetKey(window, GLFW_KEY_R) == GLFW_PRESS) {
@@ -69,39 +84,64 @@ void Player_tick(Player* player, GLFWwindow* window) {
     }
 
     if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS || glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
-        motionZ--;
+        forward--;
     }
     if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS || glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
-        motionZ++;
+        forward++;
     }
     if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS || glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
-        motionX--;
+        vertical--;
     }
     if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS || glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
-        motionX++;
+        vertical++;
     }
     if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS) {
-        Player_move(player, 0, 1, 0);
+        if (player->onGround) {
+            player->motionY = 0.12F;
+        }
     }
 
-    Player_moveRelative(player, motionX, motionZ, 0.2F);
+    // Add motion to the player using keyboard input
+    Player_moveRelative(player, vertical, forward, player->onGround ? 0.1F : 0.01F);
 
-    Player_move(player, 0, -0.5F, 0);
+    // Apply gravity
+    player->motionY -= 0.005;
+
+    // Move the player using the motion
+    Player_move(player, player->motionX, player->motionY, player->motionZ);
+    
+    // Decrease motion
+    player->motionX *= 0.91F;
+    player->motionY *= 0.98F;
+    player->motionZ *= 0.91F;
+
+    // Decrease motion on ground
+    if (player->onGround) {
+        player->motionX *= 0.8F;
+        player->motionZ *= 0.8F;
+    }
 }
+
 
 // TODO: Make this function actually work...
 void Player_moveRelative(Player* player, float x, float z, float speed) {
+    float distance = x * x + z * z;
+
+    // Stop moving if too slow
+    if (distance < 0.01F) {
+        return;
+    }
+
+    // Apply speed to relative movement
+    distance = speed / (float) sqrt(distance);
+    x *= distance;
+    z *= distance;
+
     // Calculate sin and cos of player rotation
-    float sin_val = (float) sin(player->yRotation);
-    float cos_val = (float) cos(player->yRotation);
+    double sin_val = sin(degToRad(player->yRotation));
+    double cos_val = cos(degToRad(player->yRotation));
 
     // Move the player in facing direction
-    double mX = x * cos_val - z * sin_val;
-    double mZ = z * cos_val + x * sin_val;
-
-    // Adjust speed
-    mX *= speed;
-    mZ *= speed;
-
-    Player_move(player, mX, 0, mZ);
+    player->motionX += x * cos_val - z * sin_val;
+    player->motionZ += z * cos_val + x * sin_val;
 }
--- rd-132211/src/player.h
+++ rd-132211/src/player.h
@@ -9,14 +9,16 @@
 typedef struct {
     Level* level;
     float x, y, z;
+    double motionX, motionY, motionZ;
     float yRotation, xRotation;
+    bool onGround;
     AABB boundingBox;
 } Player;
 
 void Player_init(Player* player, Level* level);
 void Player_setPosition(Player* player, float x, float y, float z);
 void Player_resetPosition(Player* player);
-void Player_turn(Player* player, float motionX, float motionY);
+void Player_turn(Player* player, float x, float y);
 void Player_move(Player* player, double x, double y, double z);
 void Player_tick(Player* player, GLFWwindow* window);
 void Player_moveRelative(Player* player, float x, float y, float speed);
